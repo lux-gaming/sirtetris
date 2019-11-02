e35d3b0fceae06104150952795788147eb0445e0
Sven Cannivy <sven.cannivy@gmail.com>
2019-11-02 16:17:42 +0100

    LICENSE, README, setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sirtetris',
+    version='0.1.0',
+    scripts=[],
+    author="Sven Cannivy",
+    author_email="sven.cannivy@gmail.com",
+    description="Tetris bot engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/svencan/sirtetris",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
